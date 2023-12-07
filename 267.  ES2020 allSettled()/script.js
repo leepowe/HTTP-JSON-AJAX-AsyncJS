5797d20ceaf2910e34ecdfd3e28ae5d0379d61ac
Lee Powell <81479565+leepowe@users.noreply.github.com>
2023-12-06 21:56:14 -0500

    setup ES2020 lesson

---
+++ 267.  ES2020 allSettled()/script.js
@@ -0,0 +1,6 @@
+const promiseOne = new Promise((resolve, reject) =>
+    setTimeout(resolve, 3000));
+const promiseTwo = new Promise((resolve, reject) =>
+    setTimeout(resolve, 3000));
+
+Promise.all([promiseOne, promiseTwo]).then(data => console.log(data));
