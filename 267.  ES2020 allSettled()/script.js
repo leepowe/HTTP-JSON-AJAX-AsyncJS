4663aef2f992ba2073d131560e18217ff0c73c20
Lee Powell <81479565+leepowe@users.noreply.github.com>
2023-12-06 22:03:00 -0500

    allSettled

--- 267.  ES2020 allSettled()/script.js
+++ 267.  ES2020 allSettled()/script.js
@@ -1,6 +1,10 @@
 const promiseOne = new Promise((resolve, reject) =>
-    setTimeout(resolve, 3000));
+    setTimeout(resolve, 6000));
 const promiseTwo = new Promise((resolve, reject) =>
-    setTimeout(resolve, 3000));
+    setTimeout(reject, 3000));
 
-Promise.all([promiseOne, promiseTwo]).then(data => console.log(data));
+Promise.all([promiseOne, promiseTwo]).then(data => console.log(data))
+    .catch( e => console.log('something failed', e));
+
+Promise.allSettled([promiseOne, promiseTwo]).then(data => console.log(data))
+    .catch( e => console.log('something failed', e));
